c8b7fed2e4c29d4001baa2de8a3a19b673d23fab
Rishi Sai Teja Ramanchi <102644611+xenon1919@users.noreply.github.com>
2023-08-14 21:44:29 +0530

    Update table.rs

--- table.rs
+++ table.rs
@@ -1,7 +1,7 @@
 fn main() {
     let x=9;
     println!("\nPRINTING THE TABLE OF 9\n");
-    for i in 1..10 {
+    for i in 1..11 {
         let n =x*i;
         println!("{} × {} = {}",x,i,n);
     }
